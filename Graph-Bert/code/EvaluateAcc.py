534fa0dff25b8b4012fef26dd588dcb891975622
RomanLesicky <romanlesickywork@gmail.com>
2026-04-07 21:57:41 +0200

    Made modifications so that sklearn is being inputted np arrays and not CUDA tensors

--- Graph-Bert/code/EvaluateAcc.py
+++ Graph-Bert/code/EvaluateAcc.py
@@ -3,16 +3,26 @@ Concrete Evaluate class for a specific evaluation metrics
 '''
 
 # Copyright (c) 2017 Jiawei Zhang <jwzhanggy@gmail.com>
-# License: TBD
+# License: MIT
 
+# Code originally modified so now CUDA tensors are correctly not being fed into sklearn but rather correct numpy arrays 
+
+import torch
 from code.base_class.evaluate import evaluate
 from sklearn.metrics import accuracy_score
 
 
 class EvaluateAcc(evaluate):
     data = None
-    
+
     def evaluate(self):
-        
-        return accuracy_score(self.data['true_y'], self.data['pred_y'])
+        true_y = self.data['true_y']
+        pred_y = self.data['pred_y']
+
+        if torch.is_tensor(true_y):
+            true_y = true_y.detach().cpu().numpy()
+        if torch.is_tensor(pred_y):
+            pred_y = pred_y.detach().cpu().numpy()
+
+        return accuracy_score(true_y, pred_y) 
         
